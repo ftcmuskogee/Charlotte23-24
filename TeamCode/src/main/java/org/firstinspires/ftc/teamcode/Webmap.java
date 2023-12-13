0c93330cd5eadc02174852a23040ebdd56923b44
Kevin Nguyen <43355408+ftcmuskogee@users.noreply.github.com>
2023-12-12 18:25:23 -0600

    adding needed stuffs
    teleop = zoom
    hardwaremap = webmap

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Webmap.java
@@ -0,0 +1,236 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class Webmap {
+    boolean y = false;
+    // names motors and sets motors to type null
+    public DcMotor Frontright = null;
+    public DcMotor Backright = null;
+    public DcMotor Backleft = null;
+    public DcMotor Frontleft = null;
+    public DcMotor LinAct = null;
+    public Servo ClawL = null;
+    public Servo ClawR = null;
+    // wrist
+    public Servo Wrist = null;
+    // linear actuator angle change
+    //may need changing
+    public Servo LinAngle = null;
+    // arm angle
+    public DcMotor ArmAngle = null;
+    public DcMotor ArmextendL = null;
+    public DcMotor ArmextendR = null;
+
+    //plane launch
+    public Servo Plane = null;
+    // sets hardware map to null and names it
+    HardwareMap Webmap = null;
+    // creates runtime variable
+    public ElapsedTime runtime = new ElapsedTime();
+
+    public void init(HardwareMap hmap) {
+        //sets up names for configuration
+        Webmap = hmap;
+
+        Frontright = hmap.get(DcMotor.class, "RF");
+        Backright = hmap.get(DcMotor.class, "RB");
+        Backleft = hmap.get(DcMotor.class, "LB");
+        Frontleft = hmap.get(DcMotor.class, "LF");
+        LinAct = hmap.get(DcMotor.class, "Act");
+        ClawL = hmap.get(Servo.class, "CL");
+        ClawR = hmap.get(Servo.class, "CR");
+        ArmextendL = hmap.get(DcMotor.class, "L");
+        ArmextendR = hmap.get(DcMotor.class, "R");
+        Wrist = hmap.get(Servo.class, "W");
+        LinAngle = hmap.get(Servo.class, "LA");
+        ArmAngle = hmap.get(DcMotor.class, "AA");
+        Plane = hmap.get(Servo.class, "P");
+
+
+        //Frontright.setDirection(DcMotor.Direction.REVERSE);
+        //Backright.setDirection(DcMotor.Direction.REVERSE);
+
+
+        // sets the lifts zeropowerbehavior to brake
+        Frontright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Backright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Frontleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Backleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        ArmextendL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArmextendL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ArmextendL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        ArmextendR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArmextendR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ArmextendR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        ArmAngle.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArmAngle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ArmAngle.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        LinAct.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LinAct.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LinAct.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+    }
+
+    // function for driving forward
+    //runs motors forward at 60% power
+    public void Forward(double seconds) {
+        runtime.reset();
+        while (runtime.milliseconds() < (seconds * 1000)) {
+            Frontleft.setPower(0.6);
+            Frontright.setPower(0.6);
+            Backleft.setPower(0.6);
+            Backright.setPower(0.6);
+        }
+    }
+
+    //function for driving backward
+    //runs motors backward at 60% power
+    public void Backward(double seconds) {
+        runtime.reset();
+        while (runtime.milliseconds() < (seconds * 1000)) {
+            Frontleft.setPower(-0.6);
+            Frontright.setPower(-0.6);
+            Backleft.setPower(-0.6);
+            Backright.setPower(-0.6);
+        }
+    }
+
+    // function for turning left
+    //runs left motors backward at 60% power
+    //runs right motors forward at 60% power
+    public void Left(double seconds) {
+        runtime.reset();
+        while (runtime.milliseconds() < (seconds * 1000)) {
+            Frontleft.setPower(-0.6);
+            Frontright.setPower(0.6);
+            Backleft.setPower(-0.6);
+            Backright.setPower(0.6);
+        }
+    }
+
+    // function for turning right
+    //runs right motors backward at 60% power
+    //runs left motors forward at 60% power
+    public void Right(double seconds) {
+        runtime.reset();
+
+        while (runtime.milliseconds() < (seconds * 1000)) {
+            Frontleft.setPower(0.6);
+            Frontright.setPower(-0.6);
+            Backleft.setPower(0.6);
+            Backright.setPower(-0.6);
+
+        }
+    }
+
+    // function for strafing right
+    //runs frontleft motor forward at 60% power
+    //runs frontright motor backward at 60% power
+    //runs backleft motor backward at 60% power
+    //runs backright motor forward at 60% power
+    public void RightStrafe(double seconds) {
+        runtime.reset();
+
+        while (runtime.milliseconds() < (seconds * 1000)) {
+            Frontleft.setPower(0.6);
+            Frontright.setPower(-0.6);
+            Backleft.setPower(-0.6);
+            Backright.setPower(0.6);
+
+        }
+    }
+
+    // function for strafing left
+    //runs frontleft motor backward at 60% power
+    //runs frontright motor forward at 60% power
+    //runs backleft motor forward at 60% power
+    //runs backright motor backward at 60% power
+    public void LeftStrafe(double seconds) {
+        runtime.reset();
+        while (runtime.milliseconds() < (seconds * 1000)) {
+            Frontleft.setPower(-0.6);
+            Frontright.setPower(0.6);
+            Backleft.setPower(0.6);
+            Backright.setPower(-0.6);
+        }
+    }
+
+    // function for turning off motors
+    public void Off() {
+        Frontright.setPower(0);
+        Frontleft.setPower(0);
+        Backleft.setPower(0);
+        Backright.setPower(0);
+    }
+
+    public void Aoff() {
+
+        ArmextendR.setPower(0);
+        ArmextendL.setPower(0);
+        ArmAngle.setPower(0);
+    }
+
+    //close both sides of claw
+    public void C(double position) {
+        ClawR.setPosition(position);
+        ClawL.setPosition(position);
+    }
+
+    //close right side claw
+    public void CR(double position) {
+        ClawR.setPosition(position);
+    }
+
+    //close left side claw
+    public void CL(double position) {
+        ClawL.setPosition(position);
+    }
+
+    //claw wrist
+    public void W(double position) {
+        Wrist.setPosition(position);
+    }
+
+    // plane launch
+    public void P(double position) {
+        Plane.setPosition(position);
+    }
+
+    public void LA(double position) {
+        LinAngle.setPosition(position);
+    }
+
+    //function for moving arm 1 into bot
+    //runs arm 1 motor in at 100% power
+    public void AD(double seconds) {
+        double time = (seconds * 1000) + runtime.milliseconds();
+        while (time > runtime.milliseconds()) {
+            ArmextendR.setPower(1);
+            ArmextendL.setPower(1);
+        }
+
+    }
+
+    //function for moving arm 1 out from bot
+    // runs arm 1 motor out at 100% power
+    public void AU(double seconds) {
+        double time = (seconds * 1000) + runtime.milliseconds();
+        while (time > runtime.milliseconds()) {
+            ArmextendR.setPower(-1);
+            ArmextendL.setPower(-1);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZOOM.java
@@ -0,0 +1,179 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+@TeleOp(name="ZOOM")
+
+public class ZOOM extends LinearOpMode {
+    // names motors and sets motors to type null
+    public DcMotor Frontright = null;
+    public DcMotor Backright = null;
+    public DcMotor Backleft = null;
+    public DcMotor Frontleft = null;
+    public DcMotor LinAct = null;
+    public Servo ClawL = null;
+    public Servo ClawR = null;
+    // wrist
+    public Servo Wrist = null;
+    // linear actuator angle change
+    //may need changing
+    public Servo LinAngle = null;
+    // arm angle
+    public DcMotor ArmAngle = null;
+    public DcMotor ArmextendL = null;
+    public DcMotor ArmextendR = null;
+
+    //plane launch
+    public Servo Plane = null;
+    // sets hardware map to null and names it
+
+    //sets booleans to false
+    private final boolean isPressed = false;
+    public boolean buttonPressed = false;
+    //sets variables to 0
+    public double lastTick = 0;
+    public double lastTime = 0;
+    public double currentTick = 0;
+    public double currentTime = 0;
+    public double targetShooterRPM = 0;
+
+
+    //calls hardware map
+    Webmap robot = new Webmap();
+
+    @Override
+    public void runOpMode() {
+        double speed;
+
+        //sets up names for configuration
+        Frontright = hardwareMap.get(DcMotor.class, "RF");
+        Backright = hardwareMap.get(DcMotor.class, "RB");
+        Backleft = hardwareMap.get(DcMotor.class, "LB");
+        Frontleft = hardwareMap.get(DcMotor.class, "LF");
+        LinAct = hardwareMap.get(DcMotor.class, "Act");
+        ClawL = hardwareMap.get(Servo.class, "CL");
+        ClawR = hardwareMap.get(Servo.class, "CR");
+        ArmextendL = hardwareMap.get(DcMotor.class, "L");
+        ArmextendR = hardwareMap.get(DcMotor.class, "R");
+        Wrist = hardwareMap.get(Servo.class, "W");
+        LinAngle = hardwareMap.get(Servo.class, "LA");
+        ArmAngle = hardwareMap.get(DcMotor.class, "AA");
+        Plane = hardwareMap.get(Servo.class, "P");
+
+        // sets the right 2 motors to reverse
+        /*Frontright.setDirection(DcMotor.Direction.REVERSE);
+        Backright.setDirection(DcMotor.Direction.REVERSE);*/
+
+        Frontright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Backright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Frontleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Backleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        ArmextendL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArmextendL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ArmextendL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        ArmextendR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArmextendR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ArmextendR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        ArmAngle.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArmAngle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ArmAngle.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        LinAct.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LinAct.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LinAct.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //calls from samplemecanumdrive
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        //sets motors to run without encoders
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //initializes hardware map
+        //robot.init(hardwareMap);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            //sets all 4 base motors to the left and right joysticks on gamepad 1
+            //uses the variables from SampleMecanumDrive to adjust motors
+            //left stick in the y direction is for going forward and backward at 80% power
+            //left stick in the x direction is for strafing left and right at 80% power
+            //right stick in the x direction is for turning left and right at 80% power
+            //was x: y
+            //was y: x
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            -gamepad1.left_stick_x * 1,
+                            gamepad1.left_stick_y * 1,
+                            -gamepad1.right_stick_x * 0.8
+                    )
+            );
+
+            drive.update();
+
+            // Makes variables Power1 and Power2 to their respective joystick
+            double Power1 = gamepad2.right_stick_y;
+            double Power2 = gamepad2.left_stick_y;
+            speed = -.2;
+            // sets the power for the lifts
+            ArmextendL.setPower(Power1 * speed);
+            ArmextendR.setPower(Power1 * speed);
+            ArmAngle.setPower(Power2 * speed);
+
+            //down
+            if (gamepad1.right_bumper) {
+                LinAct.setPower(-.2);
+            }
+            //up
+            else if (gamepad1.left_bumper) {
+                LinAct.setPower(.2);
+            } else {
+                LinAct.setPower(0);
+            }
+//turn servo not positiion
+            if (gamepad1.a) {
+                LinAngle.setPosition(0);
+            }
+            //up idk
+            if (gamepad2.right_bumper){
+                Wrist.setPosition(.65);
+            }
+            // down idk
+            if (gamepad2.left_bumper){
+                Wrist.setPosition(.8);
+            }
+            //open
+            //was .1
+
+            /**SERVO NUMBERS NEED TO BE OPPOSITE**/
+            /*
+            if (gamepad2.left_trigger >0.1) {
+                Claw.setPosition(.2);
+            }
+            if (gamepad2.right_trigger > .1){
+                Claw.setPosition(0);
+            }*/
+
+            if (gamepad2.a) {
+                Plane.setPosition(0);
+            }
+
+            //adds data to the driver hub that tells you the coordinates of where the robot is on the field
+            Pose2d poseEstimate = drive.getPoseEstimate();
+            telemetry.addData("a", poseEstimate.getX());
+            telemetry.addData("y", poseEstimate.getY());
+            telemetry.addData("heading", poseEstimate.getHeading());
+            telemetry.update();
+        }
+    }
+}
+
+
